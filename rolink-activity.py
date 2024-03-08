06f9406055c6dac668dde5b755353dadef6a1ff8
Silviu Stroe <silviu@silviu-s.com>
2024-03-08 15:16:27 +0200

    Fix ssl

--- rolink-activity.py
+++ rolink-activity.py
@@ -5,6 +5,12 @@ import json
 import threading
 from datetime import datetime
 import webbrowser
+import ssl
+import certifi
+
+# This tells Python to use the certifi bundle for SSL
+ssl._create_default_https_context = ssl._create_unverified_context
+ssl._create_default_https_context = lambda: ssl.create_default_context(cafile=certifi.where())
 
 
 class TalkerGUI:
